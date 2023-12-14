cde7a17c294237447f5b9160d5481cc2257542ae
Tomoki-Mizuno <buddyfightlovenge27@icloud.com>
2023-12-14 14:06:49 +0900

    メッセージを変更

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import modules.SayHello as App
 def run():
-    app = App.SayHello("Git")
+    app = App.SayHello("Hello, GitHub!!")
     app.say()
-    
+
 if __name__ == '__main__':
         run()
